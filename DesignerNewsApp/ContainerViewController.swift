886fa00d7478ac140261bae1a317e6af7aa426c3
James Tang <j@jamztang.com>
2015-03-17 18:14:56 +0800

    Use ScrollView in IB for swiping two VCs

---
+++ DesignerNewsApp/ContainerViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ContainerViewController.swift
+//  DesignerNewsApp
+//
+//  Created by James Tang on 17/3/15.
+//  Copyright (c) 2015 Meng To. All rights reserved.
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
